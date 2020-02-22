eeec677c47def0600c584db02bef2ecc63c23b5f
Egor Kurnev <kurnev.e@gmail.com>
2020-02-22 16:44:36 +0300

    init setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Setting up git
+git config --global user.email "kurnev.e@gmail.com"
+git config --global user.name "Egor Kurnev"
+
+# Installing apps
+# ttf-jetbrains-mono rofi neovim chromium firefox polybar 
