2e8b757b828d18dd428a8b03cfc1029440b1fbc7
egor kurnev <kurnev.e@gmail.com>
2020-02-25 09:21:59 +0300

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
+
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar example >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched..."
