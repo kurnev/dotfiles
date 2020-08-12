ade8aff34ee63d4df5e0b041c921ce6032d0a710
Kurnev Egor <kurnev.e@gmail.com>
2020-08-12 10:26:09 +0300

    init script

--- install_software.sh
+++ install_software.sh
@@ -10,10 +10,8 @@ sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfre
 git config --global user.name "Kurnev Egor"
 git config --global user.email kurnev.e@gmail.com
 
-# Enpass
-cd /etc/yum.repos.d/
-sudo wget https://yum.enpass.io/enpass-yum.repo
-sudo dnf install enpass
+# Software from official\RPM repos
+sudo dnf install sway neovim nodejs starship wl-clipboard wofi waybar fish fzf ripgrep golang
 
 # Discord
 
@@ -26,11 +24,42 @@ sudo systemctl enable --now discord-installer
 dnf copr enable pschyska/alacritty
 dnf install alacritty
 
-sudo dnf install sway neovim nodejs starship wl-clipboard
-
 # Yarn
 
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo dnf install yarn
 
-# Starship ? kubectl ? Fonts ? Init vim plug 
+# Enpass
+cd /etc/yum.repos.d/
+sudo wget https://yum.enpass.io/enpass-yum.repo
+sudo dnf install enpass
+
+# Vim
+
+nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
+nvim -es -u ~/.config/nvim/init.vim -i NONE -c "CocInstall -sync coc-json coc-tsserver coc-prettier coc-html coc-css coc-vetur coc-yaml coc-highlight coc-snippets coc-markdownlint coc-eslint coc-go coc-rls|q"
+
+# kubectl
+
+sudo touch /etc/yum.repos.d/kubernetes.repo
+sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF'
+sudo dnf install -y kubectl
+
+# Fonts
+
+cp -r ~/JetBrainsMono ~/.local/share/fonts
+fc-cache -f -v
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Color theme  ?
+
